5e68d0acee8a88cfddcffab21f8a4d9df897ab63
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-10-04 13:48:47 +0100

    fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     mode: 'all', // Removes unused font-awesome fonts and icons
-    content: ['./index.html', './dist/app.js'],
+    content: ['./index.html', './dist/elm.js'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
