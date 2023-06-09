38134e5f4c1641d0d4a49fe39267bf2de352b3c4
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-06-09 11:04:53 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Elm development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = (with pkgs.elmPackages; [ elm ]) ++ (with pkgs; [ elm2nix nodejs yarn ]);
+
+        shellHook = with pkgs.elmPackages; ''
+          echo "elm `${elm}/bin/elm --version`"
+        '';
+      };
+    });
+}
